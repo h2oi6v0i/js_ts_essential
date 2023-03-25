0396d2eb7465ddba9b3e397f44823665e8df99b7
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-03-25 22:00:07 +0900

    [#3.6][hacker-news] : 페이징 구현하기

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+    currentPage: 1,
+}
 
 /**
  * 데이터 가져오기
@@ -19,23 +22,27 @@ function getData(url) {
  */
 function newsFeed() {
     const newsFeed = getData(NEWS_URL); // 데이터 가져오고
-    const ul = document.createElement('ul');
-
     const newsList = []; // 빈 배열 만들고
 
     newsList.push('<ul>');
 
-    for (let i = 0; i < 10; i++) { // 문자열로 처리기 만들고
+    for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) { // 문자열로 처리기 만들고
         newsList.push(`
             <li>
-                <a href="#${newsFeed[i].id}">
-                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+                <a href="#/show/${newsFeed[i].id}">
+                    ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>    
             </li>
         `);
     }
 
     newsList.push('</<ul>');
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+        </div>
+    `);
 
     container.innerHTML = newsList.join(''); // 마무리
 }
@@ -44,17 +51,14 @@ function newsFeed() {
  * 글 내용 화면
  */
 function newsDetail() {
-    const id = location.hash.substr(1); // # 빼기
-
+    const id = location.hash.substr(7); // # 빼기
     // 사용자가 타이틀 클릭하면 CONTENT_URL을 가지고 ajax를 호출해서 데이터 가져옴
     const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
 
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
-
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
 }
@@ -66,6 +70,9 @@ function router() {
     const routePath = location.hash; 
 
     if (routePath === "") {
+        newsFeed();
+    } else if (routePath.indexOf("#/page/") >= 0) {
+        store.currentPage = Number(routePath.substr(7));
         newsFeed();
     } else {
         newsDetail();
