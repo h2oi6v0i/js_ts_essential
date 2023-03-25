2936b3d8389c42ed0045aae93473298221178e06
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-03-25 17:21:06 +0900

    [#3.5][hacker-news] : 라우터(화면 처리기) 만들기

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 /**
- * NOTE: 반복되는 코드 리팩토링
- * - return 쓰는 이유
- * - 이 부분은 getData 함수 자체가 쓸 부분이 아니고, getData를 사용하는 쪽에서 쓸 데이터이기 때문에!
+ * 데이터 가져오기
  */
 function getData(url) {
     ajax.open('GET', url, false); // false : 데이터를 동기적으로 처리하겠다.
@@ -16,36 +14,64 @@ function getData(url) {
     return JSON.parse(ajax.response); // 가져온 JSON 데이터를 객체로 바꾸기
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+/**
+ * 글 목록 화면
+ */
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL); // 데이터 가져오고
+    const ul = document.createElement('ul');
 
-// NOTE: 타이틀 클릭했을 때 컨텐츠 가져오기
-window.addEventListener("hashchange", function () {
-    const id = location.hash.substr(1); // # 빼기
+    const newsList = []; // 빈 배열 만들고
 
-    const newsContent = getData(CONTENT_URL.replace('@id', id));  // 사용자가 타이틀 클릭하면 CONTENT_URL을 가지고 ajax를 호출해서 데이터 가져옴
-    const title = document.createElement('h1');
+    newsList.push('<ul>');
 
-    title.innerHTML = newsContent.title;
-    
-    content.appendChild(title);
-});
+    for (let i = 0; i < 10; i++) { // 문자열로 처리기 만들고
+        newsList.push(`
+            <li>
+                <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+                </a>    
+            </li>
+        `);
+    }
 
-for (let i = 0; i < 10; i++) {
-    const div = document.createElement('div');
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+    newsList.push('</<ul>');
 
-    div.innerHTML = `
-    <li>
-        <a href="#${newsFeed[i].id}">
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>    
-    </li>
-    `;
-
-    ul.appendChild(div.firstElementChild); // === ul.appendChild(div.children[0]);
+    container.innerHTML = newsList.join(''); // 마무리
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+/**
+ * 글 내용 화면
+ */
+function newsDetail() {
+    const id = location.hash.substr(1); // # 빼기
+
+    // 사용자가 타이틀 클릭하면 CONTENT_URL을 가지고 ajax를 호출해서 데이터 가져옴
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+    const title = document.createElement('h1');
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
+
+/**
+ * 라우터
+ */
+function router() {
+    const routePath = location.hash; 
+
+    if (routePath === "") {
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
